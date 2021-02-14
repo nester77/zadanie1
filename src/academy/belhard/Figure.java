bb57d24325adeff06c0089f1cb137e91b1926732
admin <alex.nester2477@gmail.com>
2021-02-14 19:06:48 +0300

    1 commit

---
+++ src/academy/belhard/Figure.java
@@ -0,0 +1,12 @@
+package academy.belhard;
+
+public abstract class Figure {
+    protected double a;
+    protected double b;
+
+    public Figure(double a, double b) {
+      this.a=a;
+      this.b=b;
+    }
+
+}
