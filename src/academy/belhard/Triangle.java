cc3beccb27586a229d157f996f279c2ac70a8177
admin <alex.nester2477@gmail.com>
2021-02-15 18:55:41 +0300

    5 commit

--- src/academy/belhard/Triangle.java
+++ src/academy/belhard/Triangle.java
@@ -10,6 +10,22 @@ public class Triangle extends Figure implements AreaCalculator, PerimeterCalcula
         this.b = b;
     }
 
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
     @Override
     public double calculateArea() {
         return (a*b)/2;
